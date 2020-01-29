4a0fc0a5082937d8dbb882d014217aec386a6d24
Trond Makonese <trondation@gmail.com>
2020-01-28 21:39:42 -0700

    Add trun methods.

--- src/turn.js
+++ src/turn.js
@@ -3,6 +3,22 @@ class Turn {
     this.guess = guess;
     this.card = card;
   }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    return this.card;
+  }
+
+  evaluateGuess() {
+    return this.guess === this.card.correctAnswers ? true : false;
+  }
+
+  giveFeedBack() {
+    return this.evaluateGuess() ? "Correct" : "Incorrect";
+  }
 }
 
 module.exports = Turn;
